6c19a7c78ccab972bc60b842bdb9e4e7e948d6f0
TungQ25 <qtung.work@gmail.com>
2026-07-24 19:08:16 +0700

    feat(database): initialize database schema and update application properties

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot")
     id("io.spring.dependency-management")
+    id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
 
 group = "com.example.tisu"
@@ -18,11 +19,22 @@ dependencies {
     implementation("org.springframework.security:spring-security-crypto")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+openApi {
+    apiDocsUrl.set("http://localhost:8080/v3/api-docs.yaml")
+    outputDir.set(layout.projectDirectory.dir("docs"))
+    outputFileName.set("openapi.yaml")
+    waitTimeInSeconds.set(30)
 }
 
 tasks.withType<Test> {
